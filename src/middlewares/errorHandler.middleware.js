5353670cd0e99a3d6a449e6305050fd4f5407a97
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-08-31 10:35:54 -0500

    add errorHandler

---
+++ src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,10 @@
+const errorHandler = (err, req, res, next) => {
+  const httpStatus = err.status || 500
+
+  return res.status(httpStatus).json({
+    status: httpStatus,
+    message: err.message || 'Internal server error'
+  })
+}
+
+export default errorHandler
