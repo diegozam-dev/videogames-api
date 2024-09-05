a14e12a986e2c50e1f473491474ccd2c28742ac2
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-05 14:03:25 -0500

    add handling to the mongoose validation error

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,9 +1,22 @@
+import mongoose from 'mongoose'
+
 const errorHandler = (err, req, res, next) => {
-  const httpStatus = err.status || 500
+  const messages = []
+  let httpStatus
+
+  if (err instanceof mongoose.Error.ValidationError) {
+    const errKeys = Object.keys(err.errors)
+
+    errKeys.forEach(key => messages.push(err.errors[key].message))
+    httpStatus = 400
+  } else {
+    httpStatus = err.status ? err.status : 500
+    messages.push(err.message)
+  }
 
   return res.status(httpStatus).json({
     status: httpStatus,
-    message: err.message || 'Internal server error'
+    messages: messages || 'Internal server error'
   })
 }
 
