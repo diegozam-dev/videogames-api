04ab7f8967b65ec99b65255fa8b52ad8f6e7ee1f
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-05 15:26:39 -0500

    improve  age rating tests

--- tests/ageRating.test.js
+++ tests/ageRating.test.js
@@ -8,121 +8,123 @@ beforeAll(async () => {
   await connectDB()
 })
 
-describe('GET', () => {
-  test('Hacer una petición get a "v1/api/age-ratings" debería devolver una colección de age ratings.', async () => {
-    const res = await request(app).get('/v1/api/age-ratings')
+describe('Age Rating Tests', () => {
+  describe('GET', () => {
+    test('Doing a get request to “v1/api/age-ratings” should return a collection of age ratings.', async () => {
+      const res = await request(app).get('/v1/api/age-ratings')
 
-    const validAgeRating = new AgeRatingModel(res.body.data[0])
-    const error = validAgeRating.validateSync()
-
-    expect(res.status).toEqual(200)
-    expect(res.body.status).toEqual('Ok')
-    expect(Array.isArray(res.body.data)).toBe(true)
-    expect(error).toBeUndefined()
-  })
-
-  test('Hacer una petición get a "v1/api/age-ratings/66d21b1d8f8e38baa6982ee3" debería devolver un age rating.', async () => {
-    const res = await request(app).get(
-      '/v1/api/age-ratings/66d21b1d8f8e38baa6982ee3'
-    )
-
-    const validAgeRating = new AgeRatingModel(res.body.data)
-    const error = validAgeRating.validateSync()
-
-    expect(res.status).toEqual(200)
-    expect(res.body.status).toEqual('Ok')
-    expect(error).toBeUndefined()
-  })
-
-  test('Hacer una petición get a "v1/api/age-ratings/66d21b1d8f8e28baa6982ee8" debería devolver un error.', async () => {
-    const res = await request(app).get(
-      '/v1/api/age-ratings/66d21b1d8f8e28baa6982ee8'
-    )
-
-    expect(res.status).toEqual(404)
-    expect(res.body.messages[0]).toEqual(
-      'Entity, with id: 66d21b1d8f8e28baa6982ee8, does not found'
-    )
-  })
-})
-
-describe('POST', () => {
-  test('Hacer una petición post a "v1/api/age-ratings" debería devolver el age rating creado.', async () => {
-    const res = await request(app)
-      .post('/v1/api/age-ratings')
-      .send({
-        category: 'ESRB',
-        rating: 'M',
-        contentDescriptions: ['Violence', 'Blood and Gore'],
-        coverUrl: 'http://example.com/esrb-m-cover.jpg'
-      })
-      .set('Accept', 'application/json')
-
-    const createdAgeRating = res.body.data
-
-    expect(res.status).toEqual(201)
-    expect(createdAgeRating.category).toEqual('ESRB')
-    expect(createdAgeRating.rating).toEqual('M')
-    expect(createdAgeRating.contentDescriptions).toEqual([
-      'Violence',
-      'Blood and Gore'
-    ])
-    expect(createdAgeRating.coverUrl).toEqual(
-      'http://example.com/esrb-m-cover.jpg'
-    )
-  })
-
-  test('Hacer una petición post a "v1/api/age-ratings" con datos erroneos debería devolver errores de validación.', async () => {
-    const res = await request(app)
-      .post('/v1/api/age-ratings')
-      .send({
-        category:
-          '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et',
-        contentDescriptions: ['Violence', 'Blood and Gore'],
-        coverUrl: 'http://example.com/esrb-m-cover.jpg'
-      })
-      .set('Accept', 'application/json')
-
-    expect(res.status).toEqual(400)
-    expect(res.body.messages.length).toBeGreaterThan(0)
-  })
-})
-
-describe('PUT', () => {
-  test('Hacer una petición put a "v1/api/age-ratings/66da0c53e880c15d9c9a39f4" debería devolver el age rating actualizado.', async () => {
-    const res = await request(app)
-      .put('/v1/api/age-ratings/66da0c53e880c15d9c9a39f4')
-      .send({
-        category: 'ESRB',
-        rating: 'M',
-        contentDescriptions: ['Violence', 'Blood and Gore'],
-        coverUrl: 'http://example.com/esrb-m-cover.jpg'
-      })
-      .set('Accept', 'application/json')
-
-    const createdAgeRating = res.body.data
-
-    expect(res.status).toEqual(200)
-    expect(createdAgeRating._id).toEqual('66da0c53e880c15d9c9a39f4')
-    expect(createdAgeRating.category).toEqual('ESRB')
-    expect(createdAgeRating.rating).toEqual('M')
-    expect(createdAgeRating.contentDescriptions).toEqual([
-      'Violence',
-      'Blood and Gore'
-    ])
-    expect(createdAgeRating.coverUrl).toEqual(
-      'http://example.com/esrb-m-cover.jpg'
-    )
-  })
-
-  describe('DELETE', () => {
-    test('Hacer una petición delete a "v1/api/age-ratings/66da0c62630849613db35b37" debería devolver un true indicando que el documento se eliminó correctamente.', async () => {
-      const res = await request(app).delete(
-        '/v1/api/age-ratings/66da0c62630849613db35b37'
-      )
+      const validAgeRating = new AgeRatingModel(res.body.data[0])
+      const error = validAgeRating.validateSync()
 
       expect(res.status).toEqual(200)
-      expect(res.body.result).toEqual(true)
+      expect(res.body.status).toEqual('Ok')
+      expect(Array.isArray(res.body.data)).toBe(true)
+      expect(error).toBeUndefined()
+    })
+
+    test('Doing a get request to “v1/api/age-ratings/66d21b1d8f8e38baa6982ee3” should return an age rating.', async () => {
+      const res = await request(app).get(
+        '/v1/api/age-ratings/66d21b1d8f8e38baa6982ee3'
+      )
+
+      const validAgeRating = new AgeRatingModel(res.body.data)
+      const error = validAgeRating.validateSync()
+
+      expect(res.status).toEqual(200)
+      expect(res.body.status).toEqual('Ok')
+      expect(error).toBeUndefined()
+    })
+
+    test('Doing a get request to “v1/api/age-ratings/66d21b1d8f8e28baa6982ee8” should return an error because the entity does not exist.', async () => {
+      const res = await request(app).get(
+        '/v1/api/age-ratings/66d21b1d8f8e28baa6982ee8'
+      )
+
+      expect(res.status).toEqual(404)
+      expect(res.body.messages[0]).toEqual(
+        'Entity, with id: 66d21b1d8f8e28baa6982ee8, does not found'
+      )
+    })
+  })
+
+  describe('POST', () => {
+    test('Making a post request to “v1/api/age-ratings” should return the created age rating.', async () => {
+      const res = await request(app)
+        .post('/v1/api/age-ratings')
+        .send({
+          category: 'ESRB',
+          rating: 'M',
+          contentDescriptions: ['Violence', 'Blood and Gore'],
+          coverUrl: 'http://example.com/esrb-m-cover.jpg'
+        })
+        .set('Accept', 'application/json')
+
+      const createdAgeRating = res.body.data
+
+      expect(res.status).toEqual(201)
+      expect(createdAgeRating.category).toEqual('ESRB')
+      expect(createdAgeRating.rating).toEqual('M')
+      expect(createdAgeRating.contentDescriptions).toEqual([
+        'Violence',
+        'Blood and Gore'
+      ])
+      expect(createdAgeRating.coverUrl).toEqual(
+        'http://example.com/esrb-m-cover.jpg'
+      )
+    })
+
+    test('Making a post request to “v1/api/age-ratings” with wrong data should return validation errors.', async () => {
+      const res = await request(app)
+        .post('/v1/api/age-ratings')
+        .send({
+          category:
+            '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et',
+          contentDescriptions: ['Violence', 'Blood and Gore'],
+          coverUrl: 'http://example.com/esrb-m-cover.jpg'
+        })
+        .set('Accept', 'application/json')
+
+      expect(res.status).toEqual(400)
+      expect(res.body.messages.length).toBeGreaterThan(0)
+    })
+  })
+
+  describe('PUT', () => {
+    test('Doing a put request to “v1/api/age-ratings/66da0c53e880c15d9c9a39f4” should return the updated age rating.', async () => {
+      const res = await request(app)
+        .put('/v1/api/age-ratings/66da0c53e880c15d9c9a39f4')
+        .send({
+          category: 'ESRB',
+          rating: 'M',
+          contentDescriptions: ['Violence', 'Blood and Gore'],
+          coverUrl: 'http://example.com/esrb-m-cover.jpg'
+        })
+        .set('Accept', 'application/json')
+
+      const createdAgeRating = res.body.data
+
+      expect(res.status).toEqual(200)
+      expect(createdAgeRating._id).toEqual('66da0c53e880c15d9c9a39f4')
+      expect(createdAgeRating.category).toEqual('ESRB')
+      expect(createdAgeRating.rating).toEqual('M')
+      expect(createdAgeRating.contentDescriptions).toEqual([
+        'Violence',
+        'Blood and Gore'
+      ])
+      expect(createdAgeRating.coverUrl).toEqual(
+        'http://example.com/esrb-m-cover.jpg'
+      )
+    })
+
+    describe('DELETE', () => {
+      test('Doing a delete request to “v1/api/age-ratings/66da0c62630849613db35b37” should return a true indicating that the entity was successfully deleted.', async () => {
+        const res = await request(app).delete(
+          '/v1/api/age-ratings/66da0c62630849613db35b37'
+        )
+
+        expect(res.status).toEqual(200)
+        expect(res.body.result).toEqual(true)
+      })
     })
   })
 })
