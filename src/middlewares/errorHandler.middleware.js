8a34bcd5e5b81a6b8312974c4edd897043b165cc
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-09 14:43:08 -0500

    Improve code readability

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -7,22 +7,22 @@ const STATUS_CODES = {
 }
 
 const errorHandler = (err, req, res, next) => {
-  const messages = []
+  let messages
   let httpStatus
 
   if (err instanceof mongoose.Error.ValidationError) {
     const errKeys = Object.keys(err.errors)
 
-    errKeys.forEach(key => messages.push(err.errors[key].message))
+    messages = errKeys.map(key => err.errors[key].message)
     httpStatus = 400
   } else {
-    httpStatus = err.status ? err.status : 500
-    messages.push(err.message)
+    httpStatus = err.status || 500
+    messages = err.message
   }
 
   return res.status(httpStatus).json({
     status: STATUS_CODES[httpStatus],
-    messages: messages.length > 0 ? messages : 'Internal server error'
+    messages: messages || 'Internal server error'
   })
 }
 
