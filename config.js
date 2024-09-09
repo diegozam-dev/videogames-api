bec85355bbc2d5482e029e4a609450337098ae79
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-09 15:23:25 -0500

    add SECRET_JWT_KEY, USERNAME AND PASSWORD

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 import 'dotenv/config'
 
-export const { PORT = 3000, MONGODB_URI, DB_NAME } = process.env
+export const {
+  PORT = 3000,
+  MONGODB_URI,
+  DB_NAME,
+  SECRET_JWT_KEY,
+  USERNAME,
+  PASSWORD
+} = process.env
