a95d90041726bf5de5d4de4a9a450236dbe679f3
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-17 10:18:19 -0500

    add swagger definition

---
+++ swagger/index.js
@@ -0,0 +1,19 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Videogames API',
+      version: '1.0.0'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000'
+      }
+    ]
+  },
+  apis: ['./src/routes/*.routes.js', './swagger/components/*.yaml']
+}
+
+export const swaggerSpec = swaggerJSDoc(options)
