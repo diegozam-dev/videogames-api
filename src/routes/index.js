85fac6b675ce27f2a6d8948e61d9b15081dad135
diegozam-dev <128404543+diegozam-dev@users.noreply.github.com>
2024-09-17 10:19:21 -0500

    add api-docs endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import companyRouter from './company.routes.js'
 import platformRouter from './platform.routes.js'
 import gameRouter from './game.routes.js'
 import characterRouter from './character.routes.js'
+import { swaggerSpec } from '../../swagger/index.js'
+import swaggerUi from 'swagger-ui-express'
 
 const apiRoutes = Router()
 const router = Router()
@@ -15,6 +17,6 @@ apiRoutes.use('/characters', characterRouter)
 
 router.use('/', homeRoutes)
 router.use('/v1/api', apiRoutes)
-// router.use('/api-docs')
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export default router
